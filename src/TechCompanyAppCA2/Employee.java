cf481e309d8b74e10c3be0a2c4da12e76884fccf
tainarassantos <souza.tainara127@gmail.com>
2025-04-30 11:07:12 +0100

    I am starting to create my CA2 project and configure the classes for the Tech Company application.

---
+++ src/TechCompanyAppCA2/Employee.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author Tainara de Souza Santos
+ * 
+ * Employee
+ * 
+ */
+
+package TechCompanyAppCA2;
+
+public class Employee {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
